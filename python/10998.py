4f18636972b351230b398c5693e80489aba9f16e
dongdonge03 <ddonghyuke@gmail.com>
2026-03-24 11:46:56 +0900

    10998.py

---
+++ python/10998.py
@@ -0,0 +1,11 @@
+#문제 
+# 두 정수 A와 B를 입력 받은 다음, AXB를 출력 하는 프로그램을 작성하시오.
+
+#A= int(input('number= '))
+#B= int(input('number= '))
+#A,B= input().split()
+#숫자로 형변환
+a,b = input().split()
+a= int(a)
+b= int(b)
+print(a*b)
